fc2f531da0a49c208efc8f6b447bebf4473b5c5e
David Kong <davkon@gmail.com>
2021-09-25 09:58:54 +1000

    first release

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  projectRoot: path.resolve(__dirname, '../../'),
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
